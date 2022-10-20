a45e49f67ec7bb96532fbcda4f6bfc4bb94bbdea
kishor182256 <kishor.gunjal@bnt-soft.com>
2022-10-20 16:27:46 +0530

    'tsfile'

--- src/components/VueTable.vue
+++ src/components/VueTable.vue
@@ -1,49 +1,48 @@
 <template>
-  <div>
-     <vue-good-table
-            :columns="columns"
-            :rows="rows"
-            :pagination-options="{
-    enabled: true,
-    mode: 'records',
-    perPage: 5,
-    position: 'bottom',
-    perPageDropdown: [5,10],
-    dropdownAllowAll: false,
-    setCurrentPage: 2,
-    nextLabel: 'next',
-    prevLabel: 'prev',
-    rowsPerPageLabel: 'Rows per page',
-    ofLabel: 'of',
-    pageLabel: 'page', 
-    allLabel: 'All',
-  }"
-   :search-options="{
-       enabled: true,
-      
-  }"
-            ></vue-good-table>
-    
-</div>
+    <div>
+        <vue-good-table :columns="columns" :rows="rows" theme="polar-bear" :pagination-options="{
+          enabled: true,
+          mode: 'records',
+          perPage: 5,
+          position: 'bottom',
+          perPageDropdown: [5,10],
+          dropdownAllowAll: false,
+          setCurrentPage: 2,
+          nextLabel: 'next',
+          prevLabel: 'prev',
+          rowsPerPageLabel: 'Rows per page',
+          ofLabel: 'of',
+          pageLabel: 'page', 
+          allLabel: 'All',
+        }" :search-options="{
+             enabled: true,
+             position: 'middle',    
+        }"
+        
+         styleClass="vgt-table bordered">
+        </vue-good-table>
+
+    </div>
 </template>
 
 <script>
 import 'vue-good-table-next/dist/vue-good-table-next.css';
 
 export default {
-name:"VueTable",
-props:{
-    rows:{
-        type:Object
+    name: "VueTable",
+    props: {
+        rows: {
+            type: String
+        },
+        columns: {
+            type: String
+        }
     },
-    columns:{
-        type:String
-    }
-},
     components: {
-      'vue-good-table': require('vue-good-table-next').VueGoodTable
-}
-      
+        'vue-good-table': require('vue-good-table-next').VueGoodTable
+    },
+
+
 }
 </script>
 
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -6,8 +6,3 @@ declare module '*.vue' {
   export default component
 }
 
-declare module 'vue-good-table-next' {
-  import VueGoodTable from 'vue-good-table-next';
-  export  {VueGoodTable}
-}
-declare const VueGoodTable: typeof globalThis.VueGoodTable
